38bfee4dfe6f348e6f8114723e6ac48e04347a2c
Nabil Ahlhauser <nabil@bivee.co>
2016-09-25 15:59:57 -0400

    Trying to deploy/server stuff

--- server/server.js
+++ server/server.js
@@ -1,14 +1,25 @@
 const express = require('express');
 const path = require('path');
+const bodyParser = require('body-parser');
+// const MongoClient = require('mongodb').MongoClient;
 
 const app = express();
 
 let appPort = 4000;
 
+/* MongoClient.connect('link-to-mongodb', (err, database) => {
+  // ... start the server
+});*/
+
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, '/../public')));
 
 app.get('/', (res) => {
   res.sendfile(path.join(__dirname, '/../public/index.html'));
+});
+
+app.post('/quotes', (req) => {
+  console.log(req.body);
 });
 
 if (process.env.VCAP_APP_PORT !== undefined) {
